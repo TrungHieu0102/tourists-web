5c700892dba0d0081737b8668562bee5398209d0
TrungHieu0102 <hatrunghieu121@gmail.com>
2023-10-05 00:31:54 +0700

    Create role management page

--- angular/src/app/proxy/index.ts
+++ angular/src/app/proxy/index.ts
@@ -1,7 +1,8 @@
 import * as Countries from './countries';
+import * as Roles from './roles';
 import * as TourAttributes from './tour-attributes';
 import * as TourCategories from './tour-categories';
 import * as Tours from './tours';
 import * as TrungHieuTourists from './trung-hieu-tourists';
 export * from './models';
-export { Countries, TourAttributes, TourCategories, Tours, TrungHieuTourists };
+export { Countries, Roles, TourAttributes, TourCategories, Tours, TrungHieuTourists };
